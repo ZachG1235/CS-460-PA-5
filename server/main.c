6655c4d3d4d1d2d24d6bf42508e2a3abdbd05724
rmw395 <149126079+rmw395@users.noreply.github.com>
2025-11-09 16:52:17 -0700

    server main function

--- server/main.c
+++ server/main.c
@@ -6,8 +6,31 @@ reads properties, then
 implements the server loop.
 */
 
+#include "properties.h"
+#include "main.h"
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+extern void *talk_to_client(void *arg);
+
 int main() {
-    // main
+    // Load server properties
+    Properties* props = property_read_properties("server.properties");
+
+    char* portVal = property_get_property(props, "PORT");
+    int port = atoi(portVal);
+
+    initializeGlobalState();
+
+    int listenFd = createListeningSocket(port);
+    acceptLoop(listenFd);
+
+    close(listenFd);
+    return 0;
 }
 
 // prepare server data structures
