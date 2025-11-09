98f4d312d045f4c2ec409dd20dc7013b07b8d7db
Zachary Garza <ztg29@nau.edu>
2025-11-09 14:38:40 -0700

    fixed file structure

--- client/receiver_handler.c
+++ client/receiver_handler.c
@@ -1,1 +1,42 @@
-// skeleton
+
+// NAME - Avnish
+
+// include statements
+#include "message.h"
+
+/* ===== function implementation ===== */
+/*
+ * Handles incoming messages from server to client. Runs in separate thread
+ * to continuously listen for server messages and display them to user.
+ * Manages connection state and handles server commands.
+ * Returns NULL on completion.
+*/
+void *receiver_handler(void *arg) 
+{
+    // initialize variables
+        // buffer for incoming data
+        // read size variable
+    
+    // while receiving data from server socket
+        // set read size = receive data from server into buffer
+        // if read size > 0:
+            // null-terminate the received string
+            
+            // check for special server commands:
+                // if message is "SHUTDOWN ALL":
+                    // display shutdown notification
+                    // exit program successfully
+            
+            // display received message to user
+            // display prompt for next user input
+            
+        // else if read size == 0:
+            // display "server closed connection"
+            // exit program successfully
+            
+        // else if read size < 0:
+            // exit program with failure
+    
+    // return NULL
+}
+
