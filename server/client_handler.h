22812367f164b31585cd43326d070db339e2c901
Zachary Garza <ztg29@nau.edu>
2025-11-15 14:32:13 -0700

    fixed declaration and add4ed 2 more

--- server/client_handler.h
+++ server/client_handler.h
@@ -3,11 +3,18 @@
 #define clienthandler_h
 
 #include "../message.h" // import message.h
+#include "../StringUtils.c"
 
 /*
  * Big switch case function. Given a message, be able to decide 
  * what to do given a MessageStruct
 */
-void handleClient(MessageStruct messageToHandle, int socket)
+void handleClient(MessageStruct messageToHandle, int socket, Client *headPtr);
+
+// messages all clients a message
+void messageAllClients(Client *headPtr, MessageStruct messageToSend);
+
+// messages all clients besides ip and port given in parameters
+void messageAllClientsExcept(Client *headPtr, MessageStruct msgToSend, char avoidIP[20], char avoidPort[8]);
 
 #endif /* clienthandler_h */
