da6ea6aa36373d0084319478c3385a98d56fcff6
Zachary Garza <ztg29@nau.edu>
2025-11-15 13:16:19 -0700

    added message code

--- message.c
+++ message.c
@@ -11,14 +11,25 @@ ssize_t sendMessage(int socket, const void* messageToSend, size_t messageSize)
 {
     // initialize variables
         // total bytes sent
+    size_t totalSent = 0;
         // current bytes sent
+    ssize_t curBytesSent;
 
     // while the total bytes sent is less than the messageSize
+    while (totalSent < curBytesSent)
+    {
         // set current bytes sent = send(); send through socket
+        curBytesSent = send(socket, (const char*)messageToSend + totalSent, messageSize - totalSent, 0);
         // check if the connection is closed, if so return -1
+        if (curBytesSent <= 0)
+        {
+            return -1;
+        }
         // increment total bytes sent by bytes
-    
+        totalSent += curBytesSent;
+    }    
     // after loop, return the total bytes sent
+    return totalSent;
 }
 
 /*
@@ -30,12 +41,23 @@ ssize_t receiveMessage(int socket, void* messageBuffer, size_t messageSize)
 {
     // initialize variables
         // total bytes received
+    size_t totalReceived = 0;
         // current bytes
+    ssize_t curBytesReceived;
     
     // while the total bytes received is less than MessageSize
+    while (totalReceived < messageSize)
+    {
         // set current bytes = recv() ; receive throug socket
+        curBytesReceived = recv(socket, (char*)messageBuffer + totalReceived, messageSize - totalReceived, 0);
         // check if connection closed, if so return -1
+        if (curBytesReceived <= 0)
+        {
+            return curBytesReceived;
+        }
         // increment total bytes received by current bytes 
-    
+        totalReceived += curBytesReceived;
+    }
     // after loop, return the total bytes received
+    return totalReceived;
 }
